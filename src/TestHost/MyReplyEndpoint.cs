ede21ffbdb0cc1f805ef3452e5e8fe25d14cc4e4
rickdotnet <rick@rickdot.net>
2024-01-08 13:22:58 -0600

    clean-up; fix shared commands being broadcast to multiple endpoints

--- src/TestHost/MyReplyEndpoint.cs
+++ src/TestHost/MyReplyEndpoint.cs
@@ -1,13 +1,16 @@
 ﻿using Apollo.Core.Messaging.Requests;
+using Microsoft.Extensions.Logging;
 
 namespace TestHost;
 
 public record MyRequest(string Message) : IRequest<bool>;
 
-public class MyReplyEndpoint : IReplyTo<MyRequest, bool>
+public class MyReplyEndpoint(ILogger<MyReplyEndpoint> logger) : IReplyTo<MyRequest, bool>
 {
     public ValueTask<bool> HandleRequestAsync(MyRequest message, CancellationToken cancellationToken = default)
     {
+        logger.LogInformation("MyReplyEndpoint Received: {Message}", message.Message);
+        logger.LogInformation("Returning true");
         return ValueTask.FromResult(true);
     }
 }

20a2409636257412e8844f5da9e100a5fb8663dd
Rick <rick@rickdot.net>
2024-08-26 15:34:18 -0500

    team alpha, assemble

--- src/Apollo.Providers.ASB/Setup.cs
+++ src/Apollo.Providers.ASB/Setup.cs
@@ -23,19 +23,6 @@ public static class Setup
     private static TokenCredential GetCreds(AsbConfig asbConfig)
     {
         // local dev
-#if DEBUG
         return new DefaultAzureCredential() { };
-#else
-        if (string.IsNullOrEmpty(asbConfig.ClientSecret))
-            throw new Exception("Gonna be hard to get anywhere without some creds");
-        
-        // TODO: figure out which is which and then put it in asbConfig
-        var options = new TokenCredentialOptions() { AuthorityHost = new Uri("https://login.windows.net") };
-        return new ClientSecretCredential(asbConfig.TenantId, asbConfig.ClientId, asbConfig.ClientSecret, options);
-        
-        // // default with no options sets AuthorityHost to AzureAuthorityHosts.AzurePublicCloud => "https://login.microsoftonline.com/";
-        // return new ClientSecretCredential(asbConfig.TenantId, asbConfig.ClientId, asbConfig.ClientSecret);
-        // return new ClientSecretCredential();
-#endif
     }
 }
